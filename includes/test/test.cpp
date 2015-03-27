2e1e8db0e772da60ea20aeffa9190331dfbab343
kmccoy4 <kmccoy4@vols.utk.edu>
2015-03-27 12:52:37 -0400

    fixed some verbiage

---
+++ includes/test/test.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char** argv){
+    int x;
+    int y;
+    x = 10;
+    y = 16;
+    printf("x= %d, y=%d\n",x,y);
+}
